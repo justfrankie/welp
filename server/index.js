4d6815f8b627f620d476af6f9e639dfb73208552
Frankie Liu <zhuangfrankie@gmail.com>
2022-08-01 01:23:16 -0700

    implement eslint

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
-const dotenv = require('dotenv').config();
+const dotenv = require('dotenv');
+dotenv.config();
 const express = require('express');
 const app = express();
 const path = require('path');
