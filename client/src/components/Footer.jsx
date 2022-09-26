f6bc5c11a6c9a025d13ac3938254218b1f49d30b
justfrankie <zhuangfrankie@gmail.com>
2022-09-25 18:18:07 -0700

    minor css changes

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,12 +1,11 @@
 import React from "react";
-import moment from "moment";
 
 const Footer = () => (
   <span>
     <a target="_blank" href="https://www.frankiehliu.com/" id="redirectText">
       Frankie Liu
     </a>
-    <p>{`last modified: ${moment().format("MMM Do YY")}`}</p>
+    <p>{`last modified: 9/25/22`}</p>
   </span>
 );
 
